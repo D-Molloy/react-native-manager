a57086099e5d751fab0c10b7906ef9b77fde1b41
Denis Molloy <denis.molloy@gmail.com>
2019-05-13 11:28:40 -0700

    Navigation in place.  Employee create form implemented but not functional

--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,43 @@
 import React from "react";
-import { Scene, Router } from "react-native-router-flux";
+import { Scene, Router, Actions } from "react-native-router-flux";
 import LoginForm from "./components/LoginForm";
 import EmployeeList from "./components/EmployeeList";
+import EmployeeCreate from "./components/EmployeeCreate";
 
 //Top level component in our app
 const RouterComponent = () => {
   return (
     <Router>
       {/*Root Scene- Need to have a parent scene for react-native-router-flux*/}
-      <Scene key="root">
-        {/* initial - prop tells the Router to show this scene first */}
-        <Scene key="login" component={LoginForm} title="Please Login" initial />
-        <Scene key="employeeList" component={EmployeeList} title="Employees" />
+      {/* hideNavBar hides the extra nav due to nesting scenes into buckets */}
+      <Scene key="root" hideNavBar>
+        {/* Then splitting the scenes into buckets (auth/main) to prevent login button from appearing in nav*/}
+        <Scene key="auth">
+          {/* initial - prop tells the Router to show this scene first */}
+          <Scene
+            key="login"
+            component={LoginForm}
+            title="Please Login"
+            initial
+          />
+        </Scene>
+        <Scene key="main">
+          <Scene
+            key="employeeList"
+            component={EmployeeList}
+            title="Employees"
+            rightTitle="Add"
+            onRight={() => {
+              Actions.employeeCreate();
+            }}
+            initial
+          />
+          <Scene
+            key="employeeCreate"
+            component={EmployeeCreate}
+            title="Create Employee"
+          />
+        </Scene>
       </Scene>
     </Router>
   );
